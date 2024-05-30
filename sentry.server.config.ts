995800da658aae9281985b7017f3f9630216a270
Viren-45 <2000viren@gmail.com>
2024-05-30 16:42:53 -0400

    Implement auth, appwrite, sentry

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://6c4ce32c9e5e250b72e554098a5ffbb0@o4507346478825472.ingest.us.sentry.io/4507346480660480",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
--- types/index.d.ts
+++ types/index.d.ts
@@ -8,14 +8,14 @@ declare type SearchParamProps = {
 // ========================================
 
 declare type SignUpParams = {
-  firstName: string;
-  lastName: string;
-  address1: string;
-  city: string;
-  state: string;
-  postalCode: string;
-  dateOfBirth: string;
-  ssn: string;
+  firstName ?: string;
+  lastName ?: string;
+  address1 ?: string;
+  city ?: string;
+  province ?: string;
+  postalCode ?: string;
+  dateOfBirth ?: string;
+  sin ?: string;
   email: string;
   password: string;
 };
@@ -33,12 +33,13 @@ declare type User = {
   dwollaCustomerId: string;
   firstName: string;
   lastName: string;
+  name: string;
   address1: string;
   city: string;
-  state: string;
+  province: string;
   postalCode: string;
   dateOfBirth: string;
-  ssn: string;
+  sin: string;
 };
 
 declare type NewUserParams = {
@@ -210,6 +211,7 @@ declare interface TotlaBalanceBoxProps {
 
 declare interface FooterProps {
   user: User;
+  type?: 'mobile' | 'desktop'
 }
 
 declare interface RightSidebarProps {
